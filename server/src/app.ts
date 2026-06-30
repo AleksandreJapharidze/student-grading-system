a09f8a56459bb8f2c7d6001366d9dc476981e73a
Alexander Japharidze <alexanderjapharidze@gmail.com>
2026-06-30 04:01:05 +0400

    Oops. I had to remove some unnecessarry stuff from app.ts (one for testing)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,6 @@
-import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import multer from "multer";
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
@@ -15,29 +13,12 @@ import fileRouter from "./routers/file.router";
 import userRouter from "./routers/user.router";
 
 import { notFoundHandler, errorHandler } from "./middleware/error.middleware";
-import { submitAssignment } from "./services/assignment-submission.service";
 
 console.log("Environment:", process.env.NODE_ENV);
 
 const app = express();
 
-const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true,
-};
-app.use(cors(corsOptions));
 app.use(express.json());
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./uploads");
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
-    }
-});
-
-const upload = multer({ storage: storage });
 
 app.use("/api/classroom", classroomRouter);
 app.use("/api/students", studentRouter);
@@ -46,8 +27,6 @@ app.use("/api/assignments", assignmentRoute);
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/files", fileRouter);
-
-app.post("/api/assignments/:assignmentId/submissions", upload.array("files"), submitAssignment);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
