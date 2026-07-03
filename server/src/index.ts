a311fe396a336e163af126179307015be47965ca
Alexander Japharidze <alexanderjapharidze@gmail.com>
2026-07-03 12:47:48 +0400

    Allow deployed URL in cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,6 +25,7 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
+    "student-grading-system-z1dz.vercel.app",
 ];
 
 app.use(cors({
